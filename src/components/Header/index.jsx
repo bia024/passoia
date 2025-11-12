487506226086cc5cadbe3ce98a5987f8e3f568b3
Bianca-Caetano <biancacaetano079@gmail.com>
2025-11-12 19:20:32 -0300

    initial: first commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,15 @@
+export default function Header () {
+    return(
+        <header>
+            <img src="" alt="Logo Passoia" />
+            <nav>
+                <ul>
+                    <li>LOOKS</li>
+                    <li>LANÇAMENTOS</li>
+                    <li>NOVIDADE</li>
+                </ul>
+            </nav>
+        </header>
+    );
+    // posso adicionar um black friday também
+}
