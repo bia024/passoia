cce8d4f5402ec6bd826976a4b5693306d4712de6
Bianca-Caetano <biancacaetano079@gmail.com>
2025-11-24 14:04:54 -0300

    Atualizando componente Novidades

--- frontend/src/components/Novidades/Novidades.jsx
+++ frontend/src/components/Novidades/Novidades.jsx
@@ -9,9 +9,9 @@ import prod5 from "../../assets/repair-molecular.jpg";
 import prod6 from "../../assets/vitamino-spectrum.jpg";
 
 const produtosNovidades = [
-  { id: 1, imagem: prod1, nome: "Kit Batons", preco: 79.9 },
-  { id: 2, imagem: prod2, nome: "Batom Líquido", preco: 49.9 },
-  { id: 3, imagem: prod3, nome: "Gloss", preco: 99.9 },
+  { id: 1, imagem: prod1, nome: "Batons (Preço unitário)", preco: 79.9 },
+  { id: 2, imagem: prod2, nome: "Batom Líquido (Preço unitário)", preco: 49.9 },
+  { id: 3, imagem: prod3, nome: "Gloss (Preço unitário)", preco: 99.9 },
   { id: 4, imagem: prod4, nome: "Revitalift", preco: 106.99 },
   { id: 5, imagem: prod5, nome: "Absolut Repair Molecular", preco: 253.9 },
   { id: 6, imagem: prod6, nome: "Vitamino Color Spectrum", preco: 200.8 },
